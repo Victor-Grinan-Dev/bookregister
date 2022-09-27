bed03007b46f132f8b9ccfe7b00be29c6adde596
Victor-python <64462520+Victor-python@users.noreply.github.com>
2022-09-27 19:51:03 +0300

    added if(this.data) to all methods

--- bookRegister.js
+++ bookRegister.js
@@ -9,75 +9,76 @@ module.exports = class BookRegister {
     }
 
     get_total_price_of_books_by_writer(searchValue){
-        
-        if (!searchValue){
-            throw new Error('missing parameter');   
-        }else{
-            let total = 0;
-            for(let item of this.data){
-                if (item.writer === searchValue){
-                    total += item.price;
+        if(this.data){     
+            if (!searchValue){
+                throw new Error('missing parameter');   
+            }else{
+                let total = 0;
+                for(let item of this.data){
+                    if (item.writer === searchValue){
+                        total += item.price;
+                    }
                 }
-            }
-            if (total === 0){
-                throw new Error('nothing found with given');
-            }
-            return total;
-            
+                if (total === 0){
+                    throw new Error('nothing found with given');
+                }
+                return total;
+            } 
         }
     }
 
     get_All_books_By_writer(searchValue){
-    
-        if (!searchValue){
-            throw new Error('missing parameter');
-        }else{
-            const books = [];
-            for(let item of this.data){
-                if (item.writer === searchValue){
-                    books.push(item.bookname)
-                }
+        if(this.data){ 
+            if(!this.data){
+                return 'no data';
             }
-            return books;
+            if (!searchValue){
+                throw new Error('missing parameter');
+            }else{
+                const books = [];
+                for(let item of this.data){
+                    if (item.writer === searchValue){
+                        books.push(item.bookname)
+                    }
+                }
+                return books;
+            }
         }  
     }
 
     get_extras(searchKey){
-        
-        for(let item of this.data){
-            if (item.bookNumber === searchKey){
-                if (item.extras){
-                return item.extras;
+        if(this.data){
+            for(let item of this.data){
+                if (item.bookNumber === searchKey){
+                    if (item.extras){
+                    return item.extras;
+                    }
+                    return null; 
                 }
-                return null; 
             }
+
+            return null; 
         }
-        return null; 
-        
     }
     get_Price(bookNumber){
-        
-        for (let item of this.data){
-            if(item.bookNumber === bookNumber){
-                return item.price;
+        if(this.data){
+            for (let item of this.data){
+                if(item.bookNumber === bookNumber){
+                    return item.price;
+                }
             }
+            throw new Error('nothing found with given');
         }
-        throw new Error('nothing found with given');
-        
     }
 
     get_book_genres(searchKey){
-        return [];
-    }
-    /**
-     
-
-        for (let item of this.data){
-            if(item.bookNumber === searchKey){
-                return item.genres;
+        if(this.data){
+            for (let item of this.data){
+                if(item.bookNumber === searchKey){
+                    return item.genres;
+                }
             }
+            return [];
         }
-        return [];
     }
-     */
 } 
