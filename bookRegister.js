b3cde50296ea4a32526dbb2176b495d0dfcc38da
Victor-python <64462520+Victor-python@users.noreply.github.com>
2022-09-28 14:11:35 +0300

    fixed spacings

--- bookRegister.js
+++ bookRegister.js
@@ -14,75 +14,73 @@ module.exports = class BookRegister {
 
     get_total_price_of_books_by_writer(searchValue){
            
-            if (!searchValue){
-                throw new Error('missing parameter');   
-            }else{
-                let total = 0;
-                for(let item of this.data){
-                    if (item.writer === searchValue){
-                        total += item.price;
-
-                    }
+        if (!searchValue){
+            throw new Error('missingparameter');   
+        }else{
+            let total = 0;
+            for(let item of this.data){
+                if (item.writer ===searchValue){
+                    total += item.price;
                 }
-                if (total === 0){
-                    throw new Error('nothing found with given');
-                }
-                return total;
-            } 
+            }
+            if (total === 0){
+                throw new Error('nothingfound with given');
+            }
+            return total;
+        } 
     }
 
     get_All_books_By_writer(searchValue){
         
-            if(!this.data){
-                return 'no data';
-            }
-            if (!searchValue){
-                throw new Error('missing parameter');
-            }else{
-                const books = [];
-                for(let item of this.data){
-                    if (item.writer === searchValue){
-                        books.push(item.bookname)
-                    }
+        if(!this.data){
+            return 'no data';
+        }
+        if (!searchValue){
+            throw new Error('missing parameter');
+        }else{
+            const books = [];
+            for(let item of this.data){
+                if (item.writer === searchValue){
+                    books.push(item.bookname)
                 }
-                return books;
             }
-         
+            return books;
+        }
     }
 
     get_extras(searchKey){
        
-            for(let item of this.data){
-                if (item.bookNumber === searchKey){
-                    if (item.extras){
-                    return item.extras;
-                    }
-                    return null; 
+        for(let item of this.data){
+            if (item.bookNumber ===searchKey){
+                if (item.extras){
+                return item.extras;
                 }
+                return null; 
             }
+        }
 
             return null; 
        
     }
     get_Price(bookNumber){
         
-            for (let item of this.data){
-                if(item.bookNumber === bookNumber){
-                    return item.price;
-                }
+        for (let item of this.data){
+            if(item.bookNumber ===bookNumber){
+                return item.price;
             }
-            throw new Error('nothing found with given');
+        }
+        throw new Error('nothing foundwith given');
         
     }
 
     get_book_genres(searchKey){
         
-            for (let item of this.data){
-                if(item.bookNumber === searchKey){
-                    return item.genres;
-                }
+        for (let item of this.data){
+            if(item.bookNumber ===searchKey){
+                return item.genres;
             }
-            return [];
         }
+        return [];
+    }
     
 } 
